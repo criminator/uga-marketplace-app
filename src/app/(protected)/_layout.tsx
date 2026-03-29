19caf2f72550d9fd93365fd3c9e5975008dabe28
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2026-03-29 02:21:50 -0400

    sso - cleanup

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,24 +1,12 @@
-import { DarkTheme, LightTheme } from "@/constants/theme";
 import { useAppContext } from "@/context/app-context";
 import { Redirect, Slot } from "expo-router";
-import React from "react";
-import { useColorScheme } from "react-native";
-import { PaperProvider, useTheme } from "react-native-paper";
 
-export default function RootLayout() {
-    const colorScheme = useColorScheme();
-    const activeTheme = colorScheme === "dark" ? DarkTheme : LightTheme;
-    useTheme();
-
+export default function ProtectedLayout() {
     const { isAuthenticated } = useAppContext();
 
     if (!isAuthenticated) {
         return <Redirect href="/(auth)/landing" />;
     }
 
-    return (
-        <PaperProvider theme={activeTheme}>
-            <Slot />
-        </PaperProvider>
-    );
+    return <Slot />;
 }
